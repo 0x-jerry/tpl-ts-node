2e5fd353afaa3b4702fcf2e09aa0b1526188275e
Gem Xli <jie844067636@gmail.com>
2020-11-06 17:50:27 +0800

    fix: 修复 jest 匹配规则

--- jest.config.ts
+++ jest.config.ts
@@ -3,7 +3,7 @@ import type { Config } from '@jest/types'
 const conf: Config.InitialOptions = {
   preset: 'ts-jest',
   testEnvironment: 'node',
-  testMatch: ['src/__tests__/**/*.spec.ts']
+  testMatch: ['**/__tests__/**/*.spec.ts']
 }
 
 export default conf
