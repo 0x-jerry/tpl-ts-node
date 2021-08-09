1f0a04f50dbee2a7bb66ea7504aff7eee8f16a22
0x-jerry <x.jerry.wang@gmail.com>
2021-08-09 10:19:32 +0800

    perf: update tpl

--- jest.config.ts
+++ jest.config.ts
@@ -3,7 +3,7 @@ import type { Config } from '@jest/types'
 const conf: Config.InitialOptions = {
   preset: 'ts-jest',
   testEnvironment: 'node',
-  testMatch: ['**/__tests__/**/*.spec.ts']
+  testMatch: ['**/*.test.ts'],
 }
 
 export default conf
--- src/__tests__/index.spec.ts
+++
@@ -1,5 +0,0 @@
-describe('test', () => {
-  it('should true', () => {
-    expect(true).toBeTruthy()
-  })
-})
